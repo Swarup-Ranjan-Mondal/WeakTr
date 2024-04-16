d21c2f4ead764ff954de3a28ea043183e1211bf6
Swarup Ranjan Mondal <swaranmon@gmail.com>
2024-04-16 05:36:31 +0530

    more fixes

--- evaluation.py
+++ evaluation.py
@@ -141,7 +141,7 @@ def do_python_eval(predict_folder, gt_folder, name_list, num_cls=21, input_type=
                 predict_img = Image.fromarray(predict)
                 predict_img.save(os.path.join(out_dir, name + ".png"))
 
-        extension = 'jpg' if 'slum_settlements' in gt_folder else 'png'
+        extension = 'jpg' if 'slum_settlements' in str(gt_folder) else 'png'
         gt_file = os.path.join(gt_folder, f'{name}.{extension}')
         gt = np.array(Image.open(gt_file))
         cal = gt < 255
